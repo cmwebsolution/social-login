3a1e99f420d698f809b8477fe0f025c313e2f61c
Chintan M <chintance1993@gmail.com>
2025-03-12 15:44:12 +0530

    add the github credential

--- config/services.php
+++ config/services.php
@@ -46,4 +46,10 @@ return [
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => env('APP_URL').'/login/facebook/callback',
     ],
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('APP_URL').'/login/github/callback',
+    ],
 ];
